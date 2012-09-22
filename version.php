c133121299963ef924ad1c0a3a8b42ff3612c372
Geodar <david.klepacek@seznam.cz>
2012-09-22 14:07:47 +0200

    Update Some texts (and pushed to stable)

--- version.php
+++ version.php
@@ -1,4 +1,4 @@
 <?php
 // This is purely for the sake of the admin panel and update system
-define('VERSION','beta16.4');       // Current XNova Redesigned version
+define('VERSION','beta16.5');       // Current XNova Redesigned version
 ?>
