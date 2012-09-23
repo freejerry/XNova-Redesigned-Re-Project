bf01dee016b3a5b2b7cde93f7e8002cf50e8215e
Geodar <david.klepacek@seznam.cz>
2012-09-23 15:57:04 +0200

    Trying to fix LogOut

--- game/logout.php
+++ game/logout.php
@@ -6,10 +6,13 @@
  * @copyright 2008 by ?????? for XNova
  *
 */
+@include('config1.php');
+$log_out_sql = new database($dbsettings);
 getLang('login');
 setcookie($game_config['COOKIE_NAME'], "", time()-100000, "/", "", 0);
 info( "<blink>".$lang['see_you']."</blink>", $lang['session_closed'], LOGINURL);
 session_destroy();
+unset($log_out_sql);
 // -----------------------------------------------------------------------------------------------------------
 // History version
 ?>
