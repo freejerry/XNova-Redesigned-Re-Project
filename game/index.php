705c7fdc5024adbc51eb08b9ac833ad19df1263c
Geodar <david.klepacek@seznam.cz>
2013-05-19 11:00:17 +0200

    Debug it

--- game/index.php
+++ game/index.php
@@ -377,6 +377,13 @@ if($_GET['iframe'] == '1'){
 			}
       //-- Reset Officers
       require_once(ROOT_PATH . "modules/user.php");
+      //debug
+      echo "
+      <script>
+      alert(".$user->id.");
+      </script>
+      ";
+      //end
       $getofficers = doquery("SELECT off_command, off_admiral, off_engineer, off_geologist, off_technocrat, off_command_exp, off_admiral_exp, off_engineer_exp, off_geologist_exp, off_technocrat_exp FROM {{table}} WHERE `id` = '". $user->id ."';", 'users',true);
       //Commander
       if(($getofficers['off_command']==1) or ($getofficers['off_command_exp']>0))
