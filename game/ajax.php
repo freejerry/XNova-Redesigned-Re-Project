3445ec31e2840d33b8bc9a017360835314166734
Geodar <geodarcz@gmail.com>
2013-07-19 14:55:03 +0200

    Fix included in #41 This change should by working on younger php installations

--- game/ajax.php
+++ game/ajax.php
@@ -1,24 +1,24 @@
 
 <!-- Resources -->
-<input type="hidden" id="ajax_metal" value="<? echo $planetrow['metal']; ?>" />
-<input type="hidden" id="ajax_crystal" value="<? echo $planetrow['crystal']; ?>" />
-<input type="hidden" id="ajax_deuterium" value="<? echo $planetrow['deuterium']; ?>" />
+<input type="hidden" id="ajax_metal" value="<?php echo $planetrow['metal']; ?>" />
+<input type="hidden" id="ajax_crystal" value="<?php echo $planetrow['crystal']; ?>" />
+<input type="hidden" id="ajax_deuterium" value="<?php echo $planetrow['deuterium']; ?>" />
 
-<input type="hidden" id="ajax_energy_max" value="<? echo $planetrow['energy_max']; ?>" />
-<input type="hidden" id="ajax_energy_used" value="<? echo $planetrow['energy_used']; ?>" />
+<input type="hidden" id="ajax_energy_max" value="<?php echo $planetrow['energy_max']; ?>" />
+<input type="hidden" id="ajax_energy_used" value="<?php echo $planetrow['energy_used']; ?>" />
 
-<input type="hidden" id="ajax_matter" value="<? echo ($user['matter'] * DARK_MATTER_FACTOR); ?>" />
+<input type="hidden" id="ajax_matter" value="<?php echo ($user['matter'] * DARK_MATTER_FACTOR); ?>" />
 
-<input type="hidden" id="ajax_metal_max" value="<? echo $planetrow['metal_max']; ?>" />
-<input type="hidden" id="ajax_crystal_max" value="<? echo $planetrow['crystal_max']; ?>" />
-<input type="hidden" id="ajax_deuterium_max" value="<? echo $planetrow['deuterium_max']; ?>" />
+<input type="hidden" id="ajax_metal_max" value="<?php echo $planetrow['metal_max']; ?>" />
+<input type="hidden" id="ajax_crystal_max" value="<?php echo $planetrow['crystal_max']; ?>" />
+<input type="hidden" id="ajax_deuterium_max" value="<?php echo $planetrow['deuterium_max']; ?>" />
 
 <!-- Time -->
-<input type="hidden" id="ajax_time" value="<? echo time(); ?>" />
+<input type="hidden" id="ajax_time" value="<?php echo time(); ?>" />
 
 <!-- Menus update? -->
-<input type="hidden" id="ajax_menus_update" value="<? echo $user['menus_update']; ?>" />
+<input type="hidden" id="ajax_menus_update" value="<?php echo $user['menus_update']; ?>" />
 
 <!-- Request time -->
-<input type="hidden" id="ajax_request_time" value="<? echo ($loadstart - microtime(true)); ?>" />
+<input type="hidden" id="ajax_request_time" value="<?php echo ($loadstart - microtime(true)); ?>" />
 
